7b206fc56bf7f510d0601c607cc23f56e9f6b2a5
Tysheba <tmorgan2dev@gmail.com>
2018-08-06 00:16:01 -0400

    added button call function

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -1,11 +1,8 @@
- // queryURL for Giphy API
- var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=My874J3ozBuVPh6WVzLd3QAFFDDgi5NP&q=" + feeling + 
- "&limit=10&offset=0&rating=PG&lang=en"
+$(document).ready(function() {
+var buttonArr = ["happy", "sad", "joyful", "angry", "curious", "confused", "disappointed" ]
+var subject = "";
 
- var feeling = $("#feeling-input").val();
- var buttonArr = ["happy", "sad", "joyful", "angry", "curious", "confused", "disappointed" ]
-
- function setupBtn() {
+function setupBtn() {
 
   $("#buttons").empty();
   for (var i=0; i<buttonArr.length; i++) {
@@ -17,6 +14,7 @@
  }
 }
 
+
 $("#add-button").on("click", function (event) {
   event.preventDefault();
   var newFeeling = $("#input").val().trim();
@@ -24,10 +22,57 @@ $("#add-button").on("click", function (event) {
   setupBtn ();
 });
 
+$("#buttons").on("click", function() {
+  subject = $(this).attr("data-name");
+  console.log(subject);
+ 
+ var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=My874J3ozBuVPh6WVzLd3QAFFDDgi5NP&q=" + subject + 
+ "&limit=10&offset=0&rating=PG&lang=en"
+  
  $.ajax({
-   url: queryURL,
-   method: "GET"
- }).then(function(response) {
-   console.log(response);
-   $("#gifs").text(JSON.stringify(response));
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+    
+  // Storing an array of results in the results variable
+      var results = response.data;
+
+      // Looping over every result item
+      for (var i = 0; i < results.length; i++) {
+
+        // Creating a div with the class "item"
+        var gifDiv = $("<span class='item'>");
+
+        // Storing the result item's rating
+        var rating = results[i].rating;
+
+        // Creating a paragraph tag with the result item's rating
+        var p = $("<p>").text("Rating: " + rating);
+
+        // Creating an image tag
+        var gifImage = $("<img>");
+
+        // Giving the image tag a src attribute of a property pulled off the
+        // result item
+        gifImage.attr("src", results[i].images.fixed_height.url);
+
+        // Appending the paragraph and personImage we created to the "gifDiv" div we created
+        gifDiv.append(p);
+        gifDiv.append(gifImage);
+
+        // Prepending the gifDiv to the "#gifs" div in the HTML
+        $("#gifs").prepend(gifDiv);
+      
+      }
+ 
+ 
+  });
+ 
  });
+ 
+
+
+// Calling the setupBtn function to display the intial buttons
+setupBtn();
+});
